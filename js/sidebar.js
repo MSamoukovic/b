a4a55080e92738b42efb3ed50362978f38a7da87
MSamoukovic <samoukovic96@gmail.com>
2020-06-11 21:04:37 +0200

    index page

---
+++ js/sidebar.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#toggle-button").click(function () {
+        $(".nav_name").each(function () {
+            $(this).toggleClass("d-none");
+        });
+        $(".searchIcon").toggleClass("d-none");
+        $("#searchInput").toggleClass("d-flex");
+        $(".sidebar").toggleClass("bg-light border border-secondary");
+        $(".headerNutureIcon").toggleClass("d-none");
+    })
+});
